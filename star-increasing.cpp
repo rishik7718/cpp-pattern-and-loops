0f7a7c079b1760712ddd51f2ed4027dbf90bad70
Rishik <rihikkumar7718@gmail.com>
2026-07-30 09:15:31 +0530

    cpp-pattern-and-loops

---
+++ star-increasing.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,i,j;
+	cout<<"enter the number of lines of pattern u want :";
+	cin>>num;
+	for(i=1;i<=num;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+		cout <<"*";
+		}
+		cout<<endl;
+		
+	}
+}
